cf1148ae3370437fbeaa14b51e6498343ae46bd1
Tucci Trabralho <zarthurbr2@gmail.com>
2024-08-07 11:13:14 -0300

    terceiro commit

--- trash.py
+++ trash.py
@@ -6,6 +6,7 @@ import schedule
 import time
 import urllib.request
 import os
+import shutil
 
 def conectar_banco_gestor():
     db_path = 'C:\\GESTOR\\Dados\\DADOS.FDB'
@@ -79,18 +80,33 @@ def verifica_versao_nuvem():
     print(versao)
     return versao
 
+def atualiza_versao(ver_atual):
+    versao_nuvem = verifica_versao_nuvem()[0][0]  # Ajuste para acessar o valor da tupla retornada
+
+    if ver_atual != versao_nuvem:
+        url_versao_nova = 'https://raw.githubusercontent.com/Tucciland/teste_atualizar_versao/main/trash.py'
+        destino = r'C:\GESTOR\teste\trash.py'
+        pasta_inicializacao = r'C:\Users\user\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup'
+        novo_arquivo = os.path.join(pasta_inicializacao, 'trash.py')
+        
+        # Cria a pasta de destino se ela não existir
+        os.makedirs(os.path.dirname(destino), exist_ok=True)
+        
+        # Renomeia o arquivo existente, se houver
+        if os.path.exists(destino):
+            novo_nome = r'C:\GESTOR\teste\trash{}.py'.format(ver_atual)
+            os.rename(destino, novo_nome)
+            print(f'Arquivo existente renomeado para: {novo_nome}')
+        
+        # Baixa o novo arquivo
+        urllib.request.urlretrieve(url_versao_nova, destino)
+        print(f'Nova versão baixada e salva em: {destino}')
+        
+        # Move o novo arquivo para a pasta de inicialização
+        shutil.move(destino, novo_arquivo)
+        print(f'Nova versão movida para: {novo_arquivo}')
+    else:
+        print('Você já está usando a versão mais recente.')
 
 versao_atual = 1536
-versao_nuvem = verifica_versao_nuvem()
-
-if versao_atual != versao_nuvem:
-    url_versao_nova = 'https://raw.githubusercontent.com/Tucciland/teste_atualizar_versao/main/trash.py'
-    destino = r'C:\GESTOR\teste\trash.exe'
-    
-    # Cria a pasta de destino se ela não existir
-    os.makedirs(os.path.dirname(destino), exist_ok=True)
-    
-    urllib.request.urlretrieve(url_versao_nova, destino)
-    print(f'Nova versão baixada e salva em: {destino}')
-else:
-    print('Você já está usando a versão mais recente.')
+atualiza_versao(versao_atual)
